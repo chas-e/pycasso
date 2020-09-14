6c6a9abdda4f270cbc07748a2cfb7326b14de7a3
jacklmbrt07 <jacklmbrt@gmail.com>
2020-09-14 16:33:39 -0700

    imported User to model

--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
 class Art(models.Model):
     MEDIA_TYPES = (
         ('C', 'Camera Photography'),
@@ -13,7 +12,7 @@ class Art(models.Model):
         ('P', 'Painting'),
         ('S', 'Sketch Drawing'),
     )
-    artist = models.ForeignKey(User, on_delete=models.CASCADE)
+    artist = models.ForeignKey(User, on_delete=models.CASCADE)s
     title = models.CharField(max_length=100)
     id = models.AutoField(primary_key=True)
     media_type = models.CharField(max_length=1, choices=MEDIA_TYPES)
