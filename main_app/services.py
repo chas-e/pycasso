972601ee894279e988308f1c7b4a0bb888eaf1f7
chas-e <sigmapi76388@gmail.com>
2020-09-21 19:36:03 -0400

    refactoring services

--- main_app/services.py
+++ main_app/services.py
@@ -2,8 +2,6 @@ from decouple import config
 import requests
 import json
 
-from .forms import HarvardApiForm
-
 HARVARD_ART_URL = config('HARVARD_ART_URL')
 HARVARD_API_KEY = config('HARVARD_API_KEY')
 
@@ -15,4 +13,4 @@ def get_art_data(title):
     r = requests.get(url)
     print(r)
     art = r.json()
-    return {'baseimageurl': art['records'[0]['images'[0]['baseimageurl']]] , 'title': art['records'[0]['title']] }
+    return {'baseimageurl': art['records'][0]['images'][0]['baseimageurl'] , 'title': art['records'][0]['title'] }
